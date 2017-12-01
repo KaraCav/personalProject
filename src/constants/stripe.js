f570cf672a5603a1ce1239926009d6897dbe0be8
Kara Cavanaugh <karacav@msn.com>
2017-12-01 10:58:18 -0600

    Stripe fixed

--- src/constants/stripe.js
+++
@@ -1,6 +0,0 @@
-const STRIPE_PUBLISHABLE =
-  process.env.NODE_ENV === 'production'
-    ? 'pk_live_MY_PUBLISHABLE_KEY'
-    : 'pk_test_fPa0yCKDiKn7nhv42hiJQAoo';
-
-export default STRIPE_PUBLISHABLE;
